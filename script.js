b0bcdfe7e4e0bd9570ed16cb5510153591dd08ef
Donald Lee <Donald3010@outlook.com>
2022-01-28 12:21:11 -0800

    Added the Pomodoro feature

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-var type = "pomodoro"
-
 function pomodoro() {
 	minute = document.getElementById("minute");
 	second = document.getElementById("second");
 	
 	minute.innerHTML = 25;
-	second.innerHTML = 00;
-	var type = "pomodoro"
+	second.innerHTML = "00";
+
+	document.getElementById("pomodoro_button").classList.add("pomodoro_button_selected");
+	document.getElementById("short_pomodoro_button").classList.remove("pomodoro_button_selected");
+	document.getElementById("long_pomodoro_button").classList.remove("pomodoro_button_selected");
 }
 
 function short_break() {
 	minute = document.getElementById("minute");
 	second = document.getElementById("second");
 	
-	minute.innerHTML = 05;
-	second.innerHTML = 00;
-	
-	var type = "short"
+	minute.innerHTML = "05";
+	second.innerHTML = "00";
+
+	document.getElementById("pomodoro_button").classList.remove("pomodoro_button_selected");
+	document.getElementById("short_pomodoro_button").classList.add("pomodoro_button_selected");
+	document.getElementById("long_pomodoro_button").classList.remove("pomodoro_button_selected");
 }
 
 function long_break() {
@@ -24,14 +27,17 @@ function long_break() {
 	second = document.getElementById("second");
 	
 	minute.innerHTML = 15;
-	second.innerHTML = 00;
-	
-	var type = "long"
+	second.innerHTML = "00";
+
+	document.getElementById("pomodoro_button").classList.remove("pomodoro_button_selected");
+	document.getElementById("short_pomodoro_button").classList.remove("pomodoro_button_selected");
+	document.getElementById("long_pomodoro_button").classList.add("pomodoro_button_selected");
 }
 
 var seconds = 0;
 var interval ;
-function pomodoro(mins) {
+
+function run_pomodoro(mins) {
 	minute = document.getElementById("minute");
 	second = document.getElementById("second");
 	
@@ -40,11 +46,17 @@ function pomodoro(mins) {
    interval = setInterval(function() {
         seconds--;
 		
-		minute.innerHTML = seconds/60 | 0;
-		
-		second.innerHTML = seconds;
+		minute.innerHTML = ("0" + (seconds/60 | 0)).slice(-2);
+		second.innerHTML = ("0" + (seconds%60)).slice(-2);
+
         if(!seconds){
              clearInterval(interval); 
+			 if (mins == "25") {
+				 short_break();
+			 }
+			 else {
+				 pomodoro();
+			 }
         }
    },1000)
 }
@@ -52,8 +64,17 @@ function pomodoro(mins) {
 document.getElementById("start_pomodoro_button").addEventListener("click", start_pomodoro);
 
 function start_pomodoro() {
-	if (type == "pomodoro") {
-		pomodoro(25);
+	minute = document.getElementById("minute").innerHTML;
+	
+	if (minute == "25") {
+		run_pomodoro(25);
 	}
+	else if (minute == "05") {
+		run_pomodoro(5);
+	}
+	else if (minute == "15") {
+		run_pomodoro(15);
+	}
+	
 }
 
